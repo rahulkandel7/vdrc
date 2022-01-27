1562c0b8766548829c4b374cca2e7bdeaa910149
Rahul Sharma <rahulkandel7@gmail.com>
2022-01-27 18:43:05 +0545

    Added slideshows

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,19 +14,29 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
+        <!-- Animate CSS -->
+        {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/> --}}
+        <link href="{{asset('css/animate.css')}}" rel="stylesheet">
+
         <!-- Alpine JS -->
         <script src="//unpkg.com/alpinejs" defer></script>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/wow.min.js') }}" ></script>
 
         <style>
             [x-cloak] {
               display: none;
             }
         </style>
+        @yield('css')
     </head>
     <body class="font-sans antialiased">
         <x-navigation></x-navigation>
+
+        @yield('content')
+
+        @yield('js')
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,71 @@
 @extends('layouts.app')
+
+@section('css')
+     <!-- Link Swiper's CSS -->
+     <link
+     rel="stylesheet"
+     href="https://unpkg.com/swiper/swiper-bundle.min.css"
+   />
+@endsection
+
+@section('content')
+    {{-- Slideshow start --}}
+        <div class="w-full">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide relative">
+                        <img src="{{asset('images/slideshow/s1.jpeg')}}" class="h-[80vh] object-cover w-full ">
+                        <p class="absolute bottom-12 bg-zinc-100 px-4 text-zinc-700 bg-opacity-80 rounded-md shadow wow slideInLeft">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi 
+                        </p>
+                  </div>
+                  <div class="swiper-slide relative">
+                    <img src="{{asset('images/slideshow/s2.jpeg')}}" class="h-[80vh] object-cover w-full ">
+                    <p class="absolute bottom-12 bg-zinc-100 px-4 text-zinc-700 bg-opacity-80 rounded-md shadow wow slideInLeft">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi 
+                    </p>
+                  </div>
+                  <div class="swiper-slide relative">
+                    <img src="{{asset('images/slideshow/s3.jpeg')}}" class="h-[80vh] object-cover w-full ">
+                    <p class="absolute bottom-12 bg-zinc-100 px-4 text-zinc-700 bg-opacity-80 rounded-md shadow wow slideInLeft">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi 
+                    </p>
+                  </div>
+                  <div class="swiper-slide relative">
+                    <img src="{{asset('images/slideshow/s4.jpeg')}}" class="h-[80vh] object-cover w-full ">
+                    <p class="absolute bottom-12 bg-zinc-100 px-4 text-zinc-700 bg-opacity-80 rounded-md shadow wow slideInLeft">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi 
+                    </p>
+                  </div>
+                  
+                </div>
+                <div class="swiper-pagination"></div>
+              </div>
+        </div>
+    {{-- Slideshow end --}}
+@endsection
+
+@section('js')
+    <script>
+        new WOW().init();
+    </script>
+    <!-- Swiper JS -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+      var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: false,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        
+      });
+    </script>
+@endsection
